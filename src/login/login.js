de176838b4b92bc46f2900a0276da117627e5dc5
Miguel Jiménez <69486469+migue3210@users.noreply.github.com>
2024-01-21 16:48:55 -0400

    responsive adjustment

--- src/login/login.js
+++ src/login/login.js
@@ -13,15 +13,17 @@ import google_icon from '../assets/google.svg';
 export default function Login() {
     return (
         <div className="Login">
-            <div className="login-form">
-                <h2 className="title">Inicio de Sesión</h2>
-                <CustomInput label={"Email"} preffixIcon={<img src={email_icon} className="icon" alt="icon" />} type={'email'} placeholder={"mail@correo.unimet.edu.ve"}></CustomInput>
-                <CustomInput label={"Contraseña"} preffixIcon={<img src={lock_icon} className="icon" alt="icon" />} suffixIcon={<img src={eye_icon} className="icon" alt="icon" />} type={'password'} placeholder={"123"}></CustomInput>
-                <a href="" className="forgot">Olvidé mi contraseña</a>
-                <button type="button" class="send">Login</button>
-                <hr></hr>
-                <button type="button" class="google"><img src={google_icon} className="icon" alt="icon" /><p>Acceder con Google</p></button>
-                <p className="register">¿No estás registrado? <Link to="/register">Crear Cuenta</Link></p>
+            <div className="container">
+                <div className="login-form">
+                    <h2 className="title">Inicio de Sesión</h2>
+                    <CustomInput label={"Email"} preffixIcon={<img src={email_icon} className="icon" alt="icon" />} type={'email'} placeholder={"mail@correo.unimet.edu.ve"}></CustomInput>
+                    <CustomInput label={"Contraseña"} preffixIcon={<img src={lock_icon} className="icon" alt="icon" />} suffixIcon={<img src={eye_icon} className="icon" alt="icon" />} type={'password'} placeholder={"123"}></CustomInput>
+                    <a href="" className="forgot">Olvidé mi contraseña</a>
+                    <button type="button" class="send">Login</button>
+                    <hr></hr>
+                    <button type="button" class="google"><img src={google_icon} className="icon" alt="icon" /><p>Acceder con Google</p></button>
+                    <p className="register">¿No estás registrado? <Link to="/register">Crear Cuenta</Link></p>
+                </div>
             </div>
             <div className="banner">
                 <img src={saman} alt="saman" className="saman-image" />
--- src/register/register.js
+++ src/register/register.js
@@ -11,15 +11,18 @@ import university from '../assets/university.jpeg';
 export default function Register() {
     return (
         <div className="Register">
-            <div className="register-form">
-                <h2 className="title">Crear Cuenta</h2>
-                <CustomInput label={"Email"} preffixIcon={<img src={person_icon} className="icon" alt="icon" />} type={'text'} placeholder={"Pepito Pérez"}></CustomInput>
-                <CustomInput label={"Nombre"} preffixIcon={<img src={email_icon} className="icon" alt="icon" />} type={'email'} placeholder={"mail@correo.unimet.edu.ve"}></CustomInput>
-                <CustomInput label={"Teléfono"} preffixIcon={<img src={phone_icon} className="icon" alt="icon" />} type={'tel'} placeholder={"123"}></CustomInput>
-                <CustomInput label={"Contraseña"} preffixIcon={<img src={lock_icon} className="icon" alt="icon" />} suffixIcon={<img src={eye_icon} className="icon" alt="icon" />} type={'password'} placeholder={"Min. 8 caracteres"}></CustomInput>
-                <CustomInput label={"Confirmar contraseña"} preffixIcon={<img src={lock_icon} className="icon" alt="icon" />} suffixIcon={<img src={eye_icon} className="icon" alt="icon" />} type={'password'} placeholder={"Min. 8 caracteres"}></CustomInput>
-                <button type="button" class="send">Crear cuenta</button>
+            <div className="container">
+                <div className="register-form">
+                    <h2 className="title">Crear Cuenta</h2>
+                    <CustomInput label={"Email"} preffixIcon={<img src={person_icon} className="icon" alt="icon" />} type={'text'} placeholder={"Pepito Pérez"}></CustomInput>
+                    <CustomInput label={"Nombre"} preffixIcon={<img src={email_icon} className="icon" alt="icon" />} type={'email'} placeholder={"mail@correo.unimet.edu.ve"}></CustomInput>
+                    <CustomInput label={"Teléfono"} preffixIcon={<img src={phone_icon} className="icon" alt="icon" />} type={'tel'} placeholder={"123"}></CustomInput>
+                    <CustomInput label={"Contraseña"} preffixIcon={<img src={lock_icon} className="icon" alt="icon" />} suffixIcon={<img src={eye_icon} className="icon" alt="icon" />} type={'password'} placeholder={"Min. 8 caracteres"}></CustomInput>
+                    <CustomInput label={"Confirmar contraseña"} preffixIcon={<img src={lock_icon} className="icon" alt="icon" />} suffixIcon={<img src={eye_icon} className="icon" alt="icon" />} type={'password'} placeholder={"Min. 8 caracteres"}></CustomInput>
+                    <button type="button" class="send">Crear cuenta</button>
+                </div>
             </div>
+
             <div className="banner">
                 <img src={university} alt="university" className="university-image" />
             </div>
