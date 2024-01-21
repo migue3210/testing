5ad6baf90e9dc7c6067eacb5189317878a15d4f6
Miguel Jiménez <69486469+migue3210@users.noreply.github.com>
2024-01-21 15:04:00 -0400

    login layout

--- src/login/login.js
+++ src/login/login.js
@@ -5,6 +5,7 @@ import email_icon from '../assets/email.svg';
 import lock_icon from '../assets/lock.svg';
 import eye_icon from '../assets/eye.svg';
 import saman from '../assets/saman.jpg';
+import google_icon from '../assets/google.svg';
 
 export default function Login() {
     return (
@@ -14,6 +15,10 @@ export default function Login() {
                 <CustomInput label={"Email"} preffixIcon={<img src={email_icon} className="icon" alt="icon" />} type={'email'} placeholder={"mail@correo.unimet.edu.ve"}></CustomInput>
                 <CustomInput label={"Contraseña"} preffixIcon={<img src={lock_icon} className="icon" alt="icon" />} suffixIcon={<img src={eye_icon} className="icon" alt="icon" />} type={'password'} placeholder={"123"}></CustomInput>
                 <a href="" className="forgot">Olvidé mi contraseña</a>
+                <button type="button" class="send">Login</button>
+                <hr></hr>
+                <button type="button" class="google"><img src={google_icon} className="icon" alt="icon" /><p>Acceder con Google</p></button>
+                <p className="register">¿No estás registrado? <a href="">Crear Cuenta</a></p>
             </div>
             <div className="banner">
                 <img src={saman} alt="saman" className="saman-image" />
