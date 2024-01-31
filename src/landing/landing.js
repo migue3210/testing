6c0cf04ee345360e807d66d934d2b65296ac9db4
Miguel Jiménez <69486469+migue3210@users.noreply.github.com>
2024-01-31 00:01:35 -0400

    adds cursor

--- src/landing/landing.js
+++ src/landing/landing.js
@@ -7,14 +7,20 @@ import HeroImage from '../assets/rescate.jpeg'
 export default function Landing() {
     return (
         <div className="Landing">
-            <div className="filter">
-                <img src={HeroImage} alt="university" className="heroImage" />
-            </div>
-            <Header></Header>
-            <div className="hero-container">
-                <h1 className="hero-text">Conecta con las <br></br> Agrupaciones Estudiantiles</h1>
-            </div>
+            <section className="hero">
+                <div className="filter">
+                    <img src={HeroImage} alt="university" className="heroImage" />
+                </div>
+                <Header></Header>
+                <div className="caption-container">
+                    <h1 className="hero-text">Conecta con las <br></br> Agrupaciones Estudiantiles</h1>
+                </div>
+            </section>
+            <section className="content">
+
+            </section>
         </div>
+
 
     )
 }
